f619c9df3ee921d9085d44a1bc3b9a2f907e1f1a
Richard Cox <rick@bot37.com>
2018-05-22 21:55:11 -0700

    adding build

--- main.go
+++ main.go
@@ -4,19 +4,19 @@
 package main
 
 import (
-	"os"
-	"os/signal"
 	"fmt"
-	"sync"
-	"net"
-	"strings"
-	"time"
-	"math/rand"
 	log "github.com/Sirupsen/logrus"
 	"github.com/spf13/viper"
-	"gopkg.in/alexcesaro/statsd.v2"
 	"golang.org/x/net/icmp"
-    "golang.org/x/net/ipv4"
+	"golang.org/x/net/ipv4"
+	"gopkg.in/alexcesaro/statsd.v2"
+	"math/rand"
+	"net"
+	"os"
+	"os/signal"
+	"strings"
+	"sync"
+	"time"
 )
 
 const ProtocolICMP = 1
@@ -27,12 +27,12 @@ var hostChan chan checkHost
 var stopChan chan os.Signal
 
 type checkHost struct {
-	name			string
-	aRecord			*net.IPAddr
-	ttl				time.Duration
-	sleep			time.Duration
-	successMetric 	string
-	failedMetric 	string
+	name          string
+	aRecord       *net.IPAddr
+	ttl           time.Duration
+	sleep         time.Duration
+	successMetric string
+	failedMetric  string
 }
 
 // Config File Options
@@ -46,9 +46,9 @@ type checkHost struct {
 // sleep -> time.Duration (Default: '2s')
 // : Duration between ping runs
 //
-// report.host -> string 
+// report.host -> string
 // : Statsd host to report to
-//   
+//
 // report.prefix -> string
 // : Prefix for reporting metrics into statsd
 //
@@ -69,8 +69,8 @@ type checkHost struct {
 //
 // hosts.* -> map[string][]string
 // : Group of hosts that belong together.  Group names get appended to the metrics hosts.
-func readConfig(){
-	viper.SetConfigName("pingviz") 
+func readConfig() {
+	viper.SetConfigName("pingviz")
 	viper.AddConfigPath("/etc/")
 	viper.AddConfigPath("$HOME/.config/")
 	viper.AddConfigPath(".")
@@ -89,7 +89,6 @@ func readConfig(){
 		os.Exit(1)
 	}
 }
-
 
 // Return a formatted string to report back to statsd
 func metricName(host string, group string) (successful string, failed string) {
@@ -110,7 +109,7 @@ func metricName(host string, group string) (successful string, failed string) {
 		viper.GetString("report.prefix"),
 		viper.GetString("report.failed"),
 		group,
-		host, 
+		host,
 		viper.GetString("report.postfix"),
 	)
 
@@ -118,9 +117,9 @@ func metricName(host string, group string) (successful string, failed string) {
 }
 
 // Dispatcher running on a timer.
-func (c checkHost) dispatch(hostchan chan <- checkHost, close <- chan os.Signal, wg *sync.WaitGroup) {
+func (c checkHost) dispatch(hostchan chan<- checkHost, close <-chan os.Signal, wg *sync.WaitGroup) {
 	log.WithFields(log.Fields{
-		"host": c.name,
+		"host":          c.name,
 		"statsd_server": viper.GetString("report.host"),
 	}).Debug("Starting Dispatcher")
 
@@ -137,7 +136,7 @@ func (c checkHost) dispatch(hostchan chan <- checkHost, close <- chan os.Signal,
 	}
 }
 
-func ping(host <- chan checkHost, wg *sync.WaitGroup) {
+func ping(host <-chan checkHost, wg *sync.WaitGroup) {
 	log.Debug("Starting Pinger")
 
 	statsdClient, err := statsd.New(statsd.Address(viper.GetString("report.host")))
@@ -152,93 +151,92 @@ func ping(host <- chan checkHost, wg *sync.WaitGroup) {
 
 	// Start Listening for icmp replies
 	c, err := icmp.ListenPacket("ip4:icmp", ListenAddr)
-    if err != nil {
-        log.Fatal("Unable to listen for ICMP packets.  Are you running as root?")
-    }
-    defer c.Close()
+	if err != nil {
+		log.Fatal("Unable to listen for ICMP packets.  Are you running as root?")
+	}
+	defer c.Close()
 
-    for h := range host{
+	for h := range host {
 		seq := rand.Intn(65535)
 
-	    m := icmp.Message{
-	        Type: ipv4.ICMPTypeEcho, Code: 0,
-	        Body: &icmp.Echo{
-	            ID: os.Getpid() & 0xffff, Seq: seq,
-	            Data: []byte("pingviz"),
-	        },
-	    }
-	    b, err := m.Marshal(nil)
-	    if err != nil {
+		m := icmp.Message{
+			Type: ipv4.ICMPTypeEcho, Code: 0,
+			Body: &icmp.Echo{
+				ID: os.Getpid() & 0xffff, Seq: seq,
+				Data: []byte("pingviz"),
+			},
+		}
+		b, err := m.Marshal(nil)
+		if err != nil {
 			log.WithFields(log.Fields{
-				"host": h.name,
+				"host":     h.name,
 				"sequence": seq,
 			}).Warn("Unable to create ICMP packet")
 			continue
-	    }
+		}
 
-	    // Time we sent the packet
-	    start := time.Now()
+		// Time we sent the packet
+		start := time.Now()
 
-	    n, err := c.WriteTo(b, h.aRecord)
-	    if err != nil {
-	        log.WithFields(log.Fields{
-	        	"host": h.name,
-	        	"sequence": seq,
-	        }).Error("Unable to send ICMP packet")
-	        continue
-	    } else if n != len(b) {
-	        log.WithFields(log.Fields{
-	        	"host": h.name,
-	        	"sequence": seq,
-	        }).Error("Packet Size mismatch")
-	        continue
-	    }
+		n, err := c.WriteTo(b, h.aRecord)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"host":     h.name,
+				"sequence": seq,
+			}).Error("Unable to send ICMP packet")
+			continue
+		} else if n != len(b) {
+			log.WithFields(log.Fields{
+				"host":     h.name,
+				"sequence": seq,
+			}).Error("Packet Size mismatch")
+			continue
+		}
 
-	    reply := make([]byte, 1500)
-	    err = c.SetReadDeadline(time.Now().Add(h.ttl))
-	    if err != nil {
-	        log.Error("unable to set deadline")
-	    }
+		reply := make([]byte, 1500)
+		err = c.SetReadDeadline(time.Now().Add(h.ttl))
+		if err != nil {
+			log.Error("unable to set deadline")
+		}
 
-	   	READ:
-	    for {
-	    	n, peer, err := c.ReadFrom(reply) 
-	    	if err != nil {
-					duration := time.Since(start)
-					log.WithFields(log.Fields{
-						"host": h.name,
-						"duration": duration,
-						"sequence": seq,
-					}).Warn("Dropped Ping")
-					statsdClient.Increment(h.failedMetric)
-					break READ
-	    	}
+	READ:
+		for {
+			n, peer, err := c.ReadFrom(reply)
+			if err != nil {
+				duration := time.Since(start)
+				log.WithFields(log.Fields{
+					"host":     h.name,
+					"duration": duration,
+					"sequence": seq,
+				}).Warn("Dropped Ping")
+				statsdClient.Increment(h.failedMetric)
+				break READ
+			}
 
-	    	if peer.String() == h.aRecord.String()  {
-			    rm, err := icmp.ParseMessage(ProtocolICMP, reply[:n])
-			    if err != nil {
-			        log.Error("Unable to Parse ICMP Message")
-			    }
-
+			if peer.String() == h.aRecord.String() {
+				rm, err := icmp.ParseMessage(ProtocolICMP, reply[:n])
+				if err != nil {
+					log.Error("Unable to Parse ICMP Message")
+				}
 
 				switch pkt := rm.Body.(type) {
 				case *icmp.Echo:
 					if pkt.Seq == seq {
 						duration := time.Since(start)
 						log.WithFields(log.Fields{
-							"host": h.name,
+							"host":     h.name,
 							"duration": duration,
 							"sequence": seq,
 						}).Debug("Recived Ping")
 
-						statsdClient.Timing(h.successMetric, duration.Seconds() * 1e3)
+						statsdClient.Timing(h.successMetric, duration.Seconds()*1e3)
 						break READ
 					}
 				default:
 					log.Error("Unknown Error")
 				}
-	    	}
-	    }
+			}
+		}
 
 	}
 	log.Debug("Closing Pinger")
@@ -254,7 +252,6 @@ func main() {
 		"loglevel": level,
 	}).Info("Starting up PingViz")
 
-
 	hostChan = make(chan checkHost, 1)
 	go ping(hostChan, &wg)
 
@@ -263,31 +260,30 @@ func main() {
 		for _, host := range viper.GetStringSlice(config_group) {
 			successMetric, failedMetric := metricName(host, group)
 
-
 			// Resolve DNS or parse IP address
-    		dst, err := net.ResolveIPAddr("ip4", host)
-    		if err != nil {
-    			log.WithFields(log.Fields{
+			dst, err := net.ResolveIPAddr("ip4", host)
+			if err != nil {
+				log.WithFields(log.Fields{
 					"host": host,
 				}).Warn("Unable to resolve host; will not ping.")
-    			continue
-    		}
+				continue
+			}
 
 			log.WithFields(log.Fields{
-				"group": group,
-				"host": host,
-				"a_record": dst,
+				"group":          group,
+				"host":           host,
+				"a_record":       dst,
 				"success_metric": successMetric,
-				"failed_metric": failedMetric,
+				"failed_metric":  failedMetric,
 			}).Debug("Found Host")
 
 			c := checkHost{
-				name: 			host,
-				aRecord: 		dst,
-				ttl:			viper.GetDuration("ttl"),
-				sleep:			viper.GetDuration("sleep"),
-				successMetric: 	successMetric,
-				failedMetric: 	failedMetric,	
+				name:          host,
+				aRecord:       dst,
+				ttl:           viper.GetDuration("ttl"),
+				sleep:         viper.GetDuration("sleep"),
+				successMetric: successMetric,
+				failedMetric:  failedMetric,
 			}
 
 			stopChan = make(chan os.Signal, 1)
